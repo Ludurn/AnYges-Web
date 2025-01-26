b3f006491f89acdafaae5cfc8432d87cf84252cb
Ludurn <111210966+Ludurn@users.noreply.github.com>
2025-01-26 18:06:43 -0300

    v1.0.5

--- src/JS/funcoes.js
+++ src/JS/funcoes.js
@@ -1,10 +1,291 @@
+// FUNCOES PAGINA 'PERFIL'
+
+function tabVerificar() {
+    if ($("#perfilInfoBox").css('visibility') === 'visible') {
+        return "infoBox";
+    } else if ($("#perfilCupomAtivoBox").css('visibility') === 'visible') {
+        return "cupomAtivoBox";
+    } else if ($("#perfilHistCupomBox").css('visibility') === 'visible') {
+        return "histCupomBox";
+    } else if ($("#perfilHistPontoBox").css('visibility') === 'visible') {
+        return "histPontoBox";
+    }
+}
+
+function tabDadosLoader() {
+        let verifique = tabVerificar();
+        let delay = "0";
+
+        if (verifique == "cupomAtivoBox") {
+            tabCupomAtivoLoader();
+            delay = "200"; 
+        } else if (verifique == "histCupomBox") {
+            tabHistCupomLoader();
+            delay = "200";
+        } else if (verifique == "histPontoBox") {
+            tabHistPontoLoader();
+            delay = "200";
+        }
+
+
+
+        setTimeout(() => {
+            if ($("#perfilInfoBox").css('visibility') === 'hidden') {
+                $("#tabDados").css({
+                    "border-left":" #611f1f solid"
+                });
+                $("#perfilInfoBox").css(
+                    {
+                        "display": "flex"
+                    });
+                $("#perfilTitle").html("<h1 style='text-align: center;'>Dados pessoais</h1>");
+                setTimeout(() => {
+                    $("#perfilTitle").css(
+                        {
+                            "visibility": "visible",
+                            "opacity": "1",
+                            "transition": "visibility 0s, opacity 0.2s linear"
+                    });
+                    $("#perfilInfoBox").css(
+                        {
+                            "visibility": "visible",
+                            "opacity": "1",
+                            "transition": "visibility 0s, opacity 0.2s linear"
+                         });
+                    }, 100);                
+            } else {
+                $("#tabDados").css({
+                    "border-left":"white solid"
+                });
+                $("#perfilTitle").css({
+                    "opacity": "0",
+                    "transition": "visibility 0s, opacity 0.2s linear"
+                });
+                $("#perfilInfoBox").css(
+                    {
+                        "opacity": "0",
+                        "transition": "visibility 0s, opacity 0.2s linear"
+                    });
+                setTimeout(() => {
+                        $("#perfilTitle").css(
+                            {
+                                "visibility":"hidden"
+                        });
+                        $("#perfilInfoBox").css(
+                            {
+                                "visibility": "hidden",
+                                "display": "none"
+                            });
+                    }, 200);
+            }
+        }, delay);
+}
+
+function tabCupomAtivoLoader() {
+    let verifique = tabVerificar();
+    let delay = "0";
+
+    if (verifique == "infoBox") {
+        tabDadosLoader();
+        delay = "200"; 
+    } else if (verifique == "histCupomBox") {
+        tabHistCupomLoader();
+        delay = "200";
+    } else if (verifique == "histPontoBox") {
+        tabHistPontoLoader();
+        delay = "200";
+    }
+
+    setTimeout(() => {
+        if ($("#perfilCupomAtivoBox").css('visibility') === 'hidden') {
+            $("#tabCupomAtivo").css({
+                "border-left":" #611f1f solid"
+            });
+            $("#perfilCupomAtivoBox").css(
+                {
+                    "display": "flex"
+                });
+            $("#perfilTitle").html("<h1 style='text-align: center;'>Cupons Ativos</h1>");                
+            setTimeout(() => {
+                $("#perfilTitle").css(
+                    {
+                        "visibility": "visible",
+                        "opacity": "1",
+                        "transition": "visibility 0s, opacity 0.2s linear"
+                });
+                $("#perfilCupomAtivoBox").css(
+                    {
+                        "visibility": "visible",
+                        "opacity": "1",
+                        "transition": "visibility 0s, opacity 0.2s linear"
+                     });
+                }, 100);                
+        } else {
+            $("#tabCupomAtivo").css({
+                "border-left":"white solid"
+            });
+            $("#perfilTitle").css({
+                "opacity": "0",
+                "transition": "visibility 0s, opacity 0.2s linear"
+            });
+            $("#perfilCupomAtivoBox").css(
+                {
+                    "opacity": "0",
+                    "transition": "visibility 0s, opacity 0.2s linear"
+                });
+            setTimeout(() => {
+                    $("#perfilTitle").css(
+                        {
+                            "visibility":"hidden"
+                    });
+                    $("#perfilCupomAtivoBox").css(
+                        {
+                            "visibility": "hidden",
+                            "display": "none"
+                        });
+                }, 200);
+        }
+    }, delay);
+}
+
+function tabHistCupomLoader() {
+    let verifique = tabVerificar();
+    let delay = "0";
+
+    if (verifique == "infoBox") {
+        tabDadosLoader();
+        delay = "200"; 
+    } else if (verifique == "cupomAtivoBox") {
+        tabCupomAtivoLoader();
+        delay = "200";
+    } else if (verifique == "histPontoBox") {
+        tabHistPontoLoader();
+        delay = "200";
+    }
+
+    setTimeout(() => {
+        if ($("#perfilHistCupomBox").css('visibility') === 'hidden') {
+            $("#tabHistCupom").css({
+                "border-left":" #611f1f solid"
+            });
+            $("#perfilHistCupomBox").css(
+                {
+                    "display": "flex"
+                });
+            $("#perfilTitle").html("<h1 style='text-align: center;'>Histórico de cupons</h1>");
+            setTimeout(() => {
+                $("#perfilTitle").css(
+                    {
+                        "visibility": "visible",
+                        "opacity": "1",
+                        "transition": "visibility 0s, opacity 0.2s linear"
+                });
+                $("#perfilHistCupomBox").css(
+                    {
+                        "visibility": "visible",
+                        "opacity": "1",
+                        "transition": "visibility 0s, opacity 0.2s linear"
+                     });
+                }, 100);                
+        } else {
+            $("#tabHistCupom").css({
+                "border-left":"white solid"
+            });
+            $("#perfilTitle").css({
+                "opacity": "0",
+                "transition": "visibility 0s, opacity 0.2s linear"
+            });
+            $("#perfilHistCupomBox").css(
+                {
+                    "opacity": "0",
+                    "transition": "visibility 0s, opacity 0.2s linear"
+                });
+            setTimeout(() => {
+                    $("#perfilTitle").css(
+                        {
+                            "visibility":"hidden"
+                        });
+                    $("#perfilHistCupomBox").css(
+                        {
+                            "visibility": "hidden",
+                            "display": "none"
+                        });
+                }, 200);
+        }
+    }, delay);
+}
+
+function tabHistPontoLoader() {
+    let verifique = tabVerificar();
+    let delay = "0";
+
+    if (verifique == "infoBox") {
+        tabDadosLoader();
+        delay = "200"; 
+    } else if (verifique == "cupomAtivoBox") {
+        tabCupomAtivoLoader();
+        delay = "200";
+    } else if (verifique == "histCupomBox") {
+        tabHistCupomLoader();
+        delay = "200";
+    }
+
+    setTimeout(() => {
+        if ($("#perfilHistPontoBox").css('visibility') === 'hidden') {
+            $("#tabHistPonto").css({
+                "border-left":" #611f1f solid"
+            });
+            $("#perfilHistPontoBox").css(
+                {
+                    "display": "flex"
+                });
+                $("#perfilTitle").html("<h1 style='text-align: center;'>Histórico de pontos</h1>");
+            setTimeout(() => {
+                $("#perfilTitle").css(
+                    {
+                        "visibility": "visible",
+                        "opacity": "1",
+                        "transition": "visibility 0s, opacity 0.2s linear"
+                });
+                $("#perfilHistPontoBox").css(
+                    {
+                        "visibility": "visible",
+                        "opacity": "1",
+                        "transition": "visibility 0s, opacity 0.2s linear"
+                     });
+                }, 100);                
+        } else {
+            $("#tabHistPonto").css({
+                "border-left":"white solid"
+            });
+            $("#perfilTitle").css({
+                "opacity": "0",
+                "transition": "visibility 0s, opacity 0.2s linear"
+            });
+            $("#perfilHistPontoBox").css(
+                {
+                    "opacity": "0",
+                    "transition": "visibility 0s, opacity 0.2s linear"
+                });
+            setTimeout(() => {
+                    $("#perfilTitle").css(
+                        {
+                            "visibility":"hidden"
+                    });
+                    $("#perfilHistPontoBox").css(
+                        {
+                            "visibility": "hidden",
+                            "display": "none"
+                        });
+                }, 200);
+        }
+    }, delay);
+}
+
 // FUNCOES PAGINA 'HOME'
 
 function menuFadeOut() {
-    let menuDrop = document.getElementById("menuDropBG");
-    let menuDropStyle = getComputedStyle(menuDrop);
-    let visibility = menuDropStyle.getPropertyValue("visibility");
-    if (visibility === "visible") {
+    if ($("#menuDropBG").css("visibility") === "visible") {
         $("#menuDropBG").css(
             {
                 "visibility": "visible",
@@ -29,6 +310,8 @@ function menuFadeIn() {
             "transition": "visibility 0s, opacity 0.2s linear;"
         });
 }
+
+
 
 function carregarCatalogo() {
 
