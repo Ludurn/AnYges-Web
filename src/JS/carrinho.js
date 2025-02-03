4f88f4b1a14b0ea4157512b98536b64b319cc63f
Henrique Meirelles das Dores Ferraz <hmeirelles0@gmail.com>
2025-02-03 01:47:13 -0300

    Add funcionalidades carrinho
    Seguem alterações:
    - Cards do carrinho estilizados para melhor visualização (Distanciando informações e eliminando infos desnecessárias para o carrinho);
    - Adicionada função que impede a repetição de cards no carrinho;

--- src/JS/carrinho.js
+++ src/JS/carrinho.js
@@ -1,6 +1,8 @@
 let ativacao = false;
 var carrinho = document.getElementById("cartContainer");
-
+var cardPresente = false;
+var contador = 0;
+var aposPrimeiro = false;
 // Ativação/desativação do carrinho
 
 function ativarCarrinho() {
@@ -21,20 +23,31 @@ function ativarCarrinho() {
 
 var catalogoProdutos = document.getElementsByClassName("btnProdutos");
 
-// console.log(catalogoProdutos[1].innerHTML);
-
-// var colecaoCarrinho = document.get;
 for (i = 0; i < catalogoProdutos.length; i++) {
-    catalogoProdutos[i].addEventListener("click", function() {
+    catalogoProdutos[i].addEventListener("click", function adicionarCard() {
         var target = event.target;
+        //Elemento-pai que sofreu evento que chamou esta função será armazenado nesta variável abaixo
         var cardCarrinho = target.parentElement;
-        cardCarrinho.removeChild(cardCarrinho.lastElementChild);
-        carrinho.innerHTML += "<div style = 'display: flex;" + 
-        " align-items: center;" + 
-        " background-color: white;" + 
-        " width: 140%; " +
-        " transform: scale(0.7, 0.7);'>" + 
+        
+        // Impedindo cards idênticos
+        carrinho.innerHTML += "<div id = 'card" + contador + "' class = 'carrinhoCard'>" + 
         cardCarrinho.innerHTML + 
         "</div>";
+
+        var carrinhoCard = document.getElementsByClassName("carrinhoCard");
+
+        // Com a remoção da tag, a tag seguinte passa a ocupar o mesmo índice, por isso a repetição
+        carrinhoCard[carrinho.children.length - 1].removeChild(carrinhoCard[carrinho.children.length - 1].children[2]);
+        carrinhoCard[carrinho.children.length - 1].removeChild(carrinhoCard[carrinho.children.length - 1].children[2]);
+
+        // Evitando repetições de card
+        if (contador > 0) {
+            for (help = 1; help < carrinho.children.length; help++) {
+                if (carrinho.lastElementChild.innerHTML == carrinho.children[help - 1].innerHTML) {
+                    carrinho.removeChild(carrinho.lastElementChild);
+                }
+            }
+        }
+        contador++;
     });
 }
