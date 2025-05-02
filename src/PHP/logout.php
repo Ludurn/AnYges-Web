bfc39798b267a09715465d83b50636d388548231
Ludurn <111210966+Ludurn@users.noreply.github.com>
2025-05-02 20:23:05 -0300

    ajuste na seguranca do logoff

--- src/PHP/logout.php
+++ src/PHP/logout.php
@@ -1,6 +1,10 @@
 <?php
     
     session_start();
+    $_SESSION['usuario'] = "";
+    if (isset($_COOKIE['carrinho'])) {
+        setcookie("carrinho", "", strtotime("-1 week"), '/');
+    }
     session_destroy();
     $retorno = "desconectado";
     die(json_encode($retorno));
